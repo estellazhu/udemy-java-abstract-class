43885d2a7a0c1956a35cf29fcc67c9adc28cbda5
Estella <estellazhu23@gmail.com>
2018-02-09 18:37:09 -0500

    first commit

---
+++ src/com/hongzi/NodeList.java
@@ -0,0 +1,9 @@
+package com.hongzi;
+
+
+public interface NodeList {
+    ListItem getRoot();
+    boolean addItem(ListItem item);
+    boolean removeItem(ListItem item);
+    void traverse(ListItem root);
+}
